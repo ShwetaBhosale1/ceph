fec3ff4576ad5dd0ee137742f1090848601580e4
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2025-11-24 13:34:06 +0530

    mgr/cephadm: Fixed Keepalived behavior for failover
    1. Keepalived will get failover to other node when daemon node is down
    2. While removing extra daemon of Ingress service make sure to first remove it from non dependent daemon node
    
    Fixes: https://tracker.ceph.com/issues/73949
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>

--- src/pybind/mgr/cephadm/schedule.py
+++ src/pybind/mgr/cephadm/schedule.py
@@ -171,7 +171,7 @@ class DaemonPlacement(NamedTuple):
             if self.ports:
                 if self.ports != dd.ports and dd.ports:
                     return False
-                if self.ip != dd.ip and dd.ip:
+                if self.ip and dd.ip and self.ip != dd.ip:
                     return False
         return True
 
@@ -436,10 +436,31 @@ class HostAssignment(object):
             # The number of new slots that need to be selected in order to fulfill count
             need = count - len(existing)
 
-            # we don't need any additional placements
-            if need <= 0:
-                to_remove.extend(existing[count:])
-                del existing_slots[count:]
+            # Scaling down: more daemons exist than required.
+            # When related services exist, prioritize keeping daemons co-located with them
+            # by removing from non-related hosts first, then from related hosts if needed.
+            if need < 0:
+                non_matching_daemons = []
+                if self.related_service_daemons:
+                    # Get unique hostnames where related service daemons are running
+                    related_service_hosts = list(set(dd.hostname for dd in self.related_service_daemons))
+                    # Prefer removing daemons from hosts that don't have related services
+                    non_related = [dd for dd in existing if dd.hostname not in related_service_hosts]
+                    to_delete = non_related.copy()
+                    # If removing all non-related daemons still leaves us above target count,
+                    # we must also remove some daemons from hosts with related services
+                    if len(existing) - len(to_delete) > count:
+                        remaining = [dd for dd in existing if dd not in to_delete]
+                        excess = (len(existing) - len(to_delete)) - count
+                        to_delete.extend(remaining[:excess])
+                    non_matching_daemons = to_delete
+                else:
+                    # No related services - simply remove excess daemons beyond target count
+                    non_matching_daemons = existing[count:]
+
+                to_remove.extend(non_matching_daemons)
+                # remove from  existing_slots
+                existing_slots = [slot for slot in existing_slots if slot.hostname not in [dd.hostname for dd in non_matching_daemons]]
                 return self.place_per_host_daemons(existing_slots, [], to_remove)
 
             if self.related_service_daemons:
